71710bce195db30bb0c0b8246339e9e25c8877d8
Hesara2003 <it23596702@my.sliit.lk>
2025-12-01 10:47:30 +0530

    feat: export PasswordManagerPage from pages index

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -6,3 +6,4 @@ export { DownloadsPage } from './DownloadsPage';
 export { ExtensionsPage } from './ExtensionsPage';
 export { SettingsPage } from './SettingsPage';
 export { ContentFrame } from './ContentFrame';
+export { PasswordManagerPage } from './PasswordManagerPage';
